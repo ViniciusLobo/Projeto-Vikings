f6896dbe31ba9c8f4a7f600407b659b59a0b70a6
renatoff <renatoffracaroli@gmail.com>
2015-09-20 16:56:03 -0300

    Revert "conversoes pra unreal 4.9.1"
    This reverts commit 665ec02c2a2fda299f00d40cf6e42633ce0b0070.

--- Vikings_Unreal/Intermediate/Build/Win64/UE4Editor/Development/Vikings_Unreal/PCH.Vikings_Unreal.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "F:\Unreal Engine 4\Projeto-Vikings\Vikings_Unreal\Source\Vikings_Unreal\Vikings_Unreal.h"
--- Vikings_Unreal/Intermediate/Build/Win64/UE4Editor/Development/Vikings_Unreal/Vikings_Unreal.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_VIKINGS_UNREAL_H__
-#define __AUTO_VIKINGS_UNREAL_H__
-//Last Write: 19/09/2015 23:23:23 +00:00
-#include "F:\Unreal Engine 4\Projeto-Vikings\Vikings_Unreal\Source\Vikings_Unreal\Vikings_Unreal.h"
-#endif//__AUTO_VIKINGS_UNREAL_H__
